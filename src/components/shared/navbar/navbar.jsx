364eeaef3a0b8eb11ac56824c54e21725ba5f25a
Rohaan ansari <rohaanansari321@gmail.com>
2022-09-17 00:18:01 +0530

    fixed search banner height

--- src/components/shared/navbar/navbar.jsx
+++ src/components/shared/navbar/navbar.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import styles from './navbar.module.scss';
-import logo from '../../../assets/images/logo.png';
-import Dropdown from '../dropdown/dropdown';
-import Logout from '../svg/logout';
-import Orders from '../svg/orders';
-import User from '../svg/user';
-import Cart from '../svg/cart';
-import { useHistory } from 'react-router-dom';
-import ProductList from '../svg/productList';
-import { deleteAllCookies, removeCookie } from '../../../utils/cookies';
-import { getValueFromCookie } from '../../../utils/cookies';
-import default_user_avatar from '../../../assets/images/user.svg';
-import supportSvg from '../../../assets/images/help.svg';
+import React from "react";
+import styles from "./navbar.module.scss";
+import logo from "../../../assets/images/logo.png";
+import Dropdown from "../dropdown/dropdown";
+import Logout from "../svg/logout";
+import Orders from "../svg/orders";
+import User from "../svg/user";
+import Cart from "../svg/cart";
+import { useHistory } from "react-router-dom";
+import ProductList from "../svg/productList";
+import { deleteAllCookies, removeCookie } from "../../../utils/cookies";
+import { getValueFromCookie } from "../../../utils/cookies";
+import default_user_avatar from "../../../assets/images/user.svg";
+import supportSvg from "../../../assets/images/help.svg";
 
 export default function Navbar() {
-  const user = JSON.parse(getValueFromCookie('user'));
+  const user = JSON.parse(getValueFromCookie("user"));
   const history = useHistory();
   const dropdown_links = {
-    PRODUCTS: 'Products',
-    CART: 'Cart',
-    ORDERS: 'Orders',
-    PROFILE: 'Profile',
-    LOGOUT: 'Logout',
+    PRODUCTS: "Products",
+    CART: "Cart",
+    ORDERS: "Orders",
+    PROFILE: "Profile",
+    LOGOUT: "Logout",
   };
   const more_options_dropdown_options = [
     {
@@ -48,9 +48,9 @@ export default function Navbar() {
 
   async function handleLogout() {
     deleteAllCookies();
-    localStorage.removeItem('product_list');
-    localStorage.removeItem('cartItems');
-    history.replace('/');
+    localStorage.removeItem("product_list");
+    localStorage.removeItem("cartItems");
+    history.replace("/");
   }
 
   const avatar = (
@@ -74,16 +74,16 @@ export default function Navbar() {
           <img
             src={logo}
             alt="logo"
-            style={{ height: '40px', cursor: 'pointer' }}
+            style={{ height: "40px", cursor: "pointer" }}
             onClick={() => {
-              removeCookie('search_context');
-              history.push('/application');
+              removeCookie("search_context");
+              history.push("/application");
             }}
           />
           <div className="ms-auto px-3">
             <div
               className={styles.avatar_back}
-              onClick={() => history.push('/application/support')}
+              onClick={() => history.push("/application/support")}
             >
               <img
                 src={supportSvg}
@@ -98,16 +98,16 @@ export default function Navbar() {
               body_classes="dropdown-menu-right"
               click={(value) => {
                 if (value === dropdown_links.PRODUCTS) {
-                  return history.push('/application/');
+                  return history.push("/application/");
                 }
                 if (value === dropdown_links.CART) {
-                  return history.push('/application/cart');
+                  return history.push("/application/cart");
                 }
                 if (value === dropdown_links.ORDERS) {
-                  return history.push('/application/orders');
+                  return history.push("/application/orders");
                 }
                 if (value === dropdown_links.PROFILE) {
-                  return history.push('/application/profile');
+                  return history.push("/application/profile");
                 }
                 if (value === dropdown_links.LOGOUT) {
                   return handleLogout();
