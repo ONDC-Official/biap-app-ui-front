4b2050af853fdff70e1494d8f1304ea1fff94ea9
Abhishek Singh <singh.abhishek@ondc.org>
2023-06-27 21:33:36 +0530

    Fix: RATS-257,284

--- src/components/application/my-tickets/myTickets.jsx
+++ src/components/application/my-tickets/myTickets.jsx
@@ -17,6 +17,7 @@ import Pagination from "../../shared/pagination/pagination";
 import { toast_actions, toast_types } from "../../shared/toast/utils/toast";
 import { ToastContext } from "../../../context/toastContext";
 import useCancellablePromise from "../../../api/cancelRequest";
+import MyTicket from "../../../components/shared/svg/my-tickets";
 
 export default function MyTickets() {
     // STATES
@@ -65,7 +66,7 @@ export default function MyTickets() {
     }, [pagination.currentPage, pagination.postPerPage]);
 
     useEffect(() => {
-        getAllTickets();
+        // getAllTickets();
     }, [getAllTickets, pagination.currentPage]);
 
     // loading UI
@@ -84,11 +85,7 @@ export default function MyTickets() {
         >
             <div className="text-center">
                 <div className="py-2">
-                    <img
-                        src={no_result_empty_illustration}
-                        alt="empty_search"
-                        style={{ height: "130px" }}
-                    />
+                    <MyTicket width={120} height={120} />
                 </div>
                 <div className="py-2">
                     <p className={styles.illustration_header}>No Complaints found!</p>
