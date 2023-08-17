1851559be07979c3b371b46b14cc5888911c52c6
vishal-wemotive <vishal@wemotiveforge.com>
2023-08-17 13:58:33 +0530

    add: MUI theme overriding for card.

--- src/utils/Theme/components/MuiCard.js
+++ src/utils/Theme/components/MuiCard.js
@@ -3,8 +3,9 @@ import palette from "../palette";
 const MuiCard = {
     styleOverrides: {
         root: {
-            borderRadius: '10px !important',
-            background: palette.common.white
+            borderRadius: '16px !important',
+            background: palette.common.white,
+            boxShadow: '0px 0px 0px 0px rgba(0, 0, 0, 0.05), 0px 6px 13px 0px rgba(0, 0, 0, 0.05), 0px 24px 24px 0px rgba(0, 0, 0, 0.04), 0px 54px 33px 0px rgba(0, 0, 0, 0.03), 0px 97px 39px 0px rgba(0, 0, 0, 0.01), 0px 151px 42px 0px rgba(0, 0, 0, 0.00)'
         }
     }
 };
