2a7731604d8f454ce4a881ec738342e35d1212a9
Rohaan ansari <rohaanansari321@gmail.com>
2023-10-20 14:43:22 +0530

    add: lat n lng to init req payload

--- src/components/checkout/stepPayment/stepPaymentContent.js
+++ src/components/checkout/stepPayment/stepPaymentContent.js
@@ -33,8 +33,7 @@ const StepPaymentContent = ({
 }) => {
   const classes = useStyles();
 
-  const { deliveryAddress, billingAddress, setBillingAddress } =
-    useContext(AddressContext);
+  const { deliveryAddress, billingAddress, setBillingAddress } = useContext(AddressContext);
 
   const transaction_id = getValueFromCookie("transaction_id");
   const latLongInfo = JSON.parse(Cookies.get("LatLongInfo") || "{}");
@@ -90,10 +89,7 @@ const StepPaymentContent = ({
       const { message } = item;
       checkoutObj = {
         productQuotes: [...checkoutObj.productQuotes, message?.order?.quote],
-        successOrderIds: [
-          ...checkoutObj.successOrderIds,
-          message?.order?.provider?.id.toString(),
-        ],
+        successOrderIds: [...checkoutObj.successOrderIds, message?.order?.provider?.id.toString()],
       };
     });
     // AddCookie("checkout_details", JSON.stringify(checkoutObj));
@@ -104,9 +100,7 @@ const StepPaymentContent = ({
     setInitializeOrderLoading(true);
     try {
       localStorage.setItem("selectedItems", JSON.stringify(updatedCartItems));
-      const data = await cancellablePromise(
-        getCall(`/clientApis/v2/on_initialize_order?messageIds=${message_id}`)
-      );
+      const data = await cancellablePromise(getCall(`/clientApis/v2/on_initialize_order?messageIds=${message_id}`));
       responseRef.current = [...responseRef.current, data[0]];
       setEventData((eventData) => [...eventData, data[0]]);
 
@@ -152,17 +146,12 @@ const StepPaymentContent = ({
         // check if all the orders got cancled
         if (responseRef.current.length <= 0) {
           setInitializeOrderLoading(false);
-          dispatchToast(
-            toast_types.error,
-            "Cannot fetch details for this product Please try again!"
-          );
+          dispatchToast(toast_types.error, "Cannot fetch details for this product Please try again!");
           return;
         }
         // tale action to redirect them.
         const requestObject = constructQouteObject(
-          updatedCartItems.filter(({ provider }) =>
-            responseReceivedIds.includes(provider.id.toString())
-          )
+          updatedCartItems.filter(({ provider }) => responseReceivedIds.includes(provider.id.toString()))
         );
         if (requestObject.length !== responseRef.current.length) {
           dispatchToast(toast_types.error, "Some orders are not initialized!");
@@ -196,10 +185,9 @@ const StepPaymentContent = ({
               itemData.fulfillment_id = selectedFulfillmemtId;
               delete itemData.product.fulfillment_id;
               if (updatedCartItems.current) {
-                let findItemFromQuote =
-                  updatedCartItems.current[0].message.quote.items.find(
-                    (data) => data.id === itemData.local_id
-                  );
+                let findItemFromQuote = updatedCartItems.current[0].message.quote.items.find(
+                  (data) => data.id === itemData.local_id
+                );
                 if (findItemFromQuote) {
                   itemData.parent_item_id = findItemFromQuote.parent_item_id;
                 }
@@ -218,9 +206,7 @@ const StepPaymentContent = ({
               },
               message: {
                 items: itemsData,
-                fulfillments: fulfillments.filter(
-                  (fulfillment) => fulfillment.id === selectedFulfillmemtId
-                ),
+                fulfillments: fulfillments.filter((fulfillment) => fulfillment.id === selectedFulfillmemtId),
                 billing_info: {
                   address: removeNullValues(billingAddress?.address),
                   phone: billingAddress?.phone,
@@ -233,15 +219,12 @@ const StepPaymentContent = ({
                   email: deliveryAddress?.email,
                   phone: deliveryAddress?.phone,
                   location: {
-                    gps: `${latLongInfo?.latitude}, ${latLongInfo?.longitude}`,
+                    gps: `${deliveryAddress?.location?.address?.lat}, ${deliveryAddress?.location?.address?.lng}`,
                     ...deliveryAddress?.location,
                   },
                 },
                 payment: {
-                  type:
-                    activePaymentMethod === payment_methods.COD
-                      ? "ON-FULFILLMENT"
-                      : "ON-ORDER",
+                  type: activePaymentMethod === payment_methods.COD ? "ON-FULFILLMENT" : "ON-ORDER",
                 },
               },
             };
@@ -249,9 +232,7 @@ const StepPaymentContent = ({
         )
       );
       //Error handling workflow eg, NACK
-      const isNACK = data.find(
-        (item) => item.error && item.message.ack.status === "NACK"
-      );
+      const isNACK = data.find((item) => item.error && item.message.ack.status === "NACK");
       if (isNACK) {
         dispatchToast(toast_types.error, isNACK.error.message);
         setInitializeOrderLoading(false);
@@ -268,10 +249,7 @@ const StepPaymentContent = ({
         // store parent order id to cookies
         AddCookie("parent_order_id", data[0]?.context?.parent_order_id);
         // store the map into cookies
-        AddCookie(
-          "parent_and_transaction_id_map",
-          JSON.stringify(Array.from(parentTransactionIdMap.entries()))
-        );
+        AddCookie("parent_and_transaction_id_map", JSON.stringify(Array.from(parentTransactionIdMap.entries())));
         onInit(
           data?.map((txn) => {
             const { context } = txn;
@@ -293,9 +271,7 @@ const StepPaymentContent = ({
       return item.item;
     });
     const request_object = constructQouteObject(
-      c.filter(({ provider }) =>
-        responseReceivedIds.includes(provider.local_id.toString())
-      )
+      c.filter(({ provider }) => responseReceivedIds.includes(provider.local_id.toString()))
     );
 
     initializeOrder(request_object);
@@ -305,16 +281,11 @@ const StepPaymentContent = ({
     <Grid container spacing={3}>
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
         <Card
-          className={`${classes.paymentCard} ${
-            activePaymentMethod === payment_methods.COD
-              ? classes.activeCard
-              : ""
-          } ${initializeOrderLoading ? classes.nonClickable : ""}`}
+          className={`${classes.paymentCard} ${activePaymentMethod === payment_methods.COD ? classes.activeCard : ""} ${
+            initializeOrderLoading ? classes.nonClickable : ""
+          }`}
           onClick={() => {
-            if (
-              !initializeOrderLoading &&
-              activePaymentMethod !== payment_methods.COD
-            ) {
+            if (!initializeOrderLoading && activePaymentMethod !== payment_methods.COD) {
               setActivePaymentMethod(payment_methods.COD);
               handleInitializaOrder();
             }
@@ -322,30 +293,19 @@ const StepPaymentContent = ({
         >
           {/*<img className={classes.paymentImage} src={cashOnDelivery} alt="Cash on delivery"/>*/}
           <CashOnDelivery className={classes.paymentImage} />
-          {activePaymentMethod === payment_methods.COD && (
-            <CheckedIcon className={classes.checkedIcon} />
-          )}
+          {activePaymentMethod === payment_methods.COD && <CheckedIcon className={classes.checkedIcon} />}
         </Card>
-        <Typography
-          className={classes.paymentTypo}
-          variant="body"
-          component="div"
-        >
+        <Typography className={classes.paymentTypo} variant="body" component="div">
           Cash on delivery
         </Typography>
       </Grid>
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
         <Card
           className={`${classes.paymentCard} ${
-            activePaymentMethod === payment_methods.JUSPAY
-              ? classes.activeCard
-              : ""
+            activePaymentMethod === payment_methods.JUSPAY ? classes.activeCard : ""
           } ${initializeOrderLoading ? classes.nonClickable : ""}`}
           onClick={() => {
-            if (
-              !initializeOrderLoading &&
-              activePaymentMethod !== payment_methods.JUSPAY
-            ) {
+            if (!initializeOrderLoading && activePaymentMethod !== payment_methods.JUSPAY) {
               setActivePaymentMethod(payment_methods.JUSPAY);
               handleInitializaOrder();
             }
@@ -353,15 +313,9 @@ const StepPaymentContent = ({
         >
           {/*<img className={classes.paymentImage} src={prepaid} alt="Prepaid"/>*/}
           <Prepaid className={classes.paymentImage} />
-          {activePaymentMethod === payment_methods.JUSPAY && (
-            <CheckedIcon className={classes.checkedIcon} />
-          )}
+          {activePaymentMethod === payment_methods.JUSPAY && <CheckedIcon className={classes.checkedIcon} />}
         </Card>
-        <Typography
-          className={classes.paymentTypo}
-          variant="body"
-          component="div"
-        >
+        <Typography className={classes.paymentTypo} variant="body" component="div">
           Prepaid
         </Typography>
       </Grid>
