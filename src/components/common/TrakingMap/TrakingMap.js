1209df3245b2a8b209f746b3c001bb9d7e466a52
vishal-wemotive <vishal@wemotiveforge.com>
2023-12-01 17:38:27 +0530

    fix: dynamic tracking functionality added.

--- src/components/common/TrakingMap/TrakingMap.js
+++ src/components/common/TrakingMap/TrakingMap.js
@@ -15,96 +15,54 @@ export default function TrakingMapComponent(props) {
     currentLocation = null
   } = props;
 
-  console.log("mapCenter=====>", mapCenter(), geoPositionStart);
-
   const { cancellablePromise } = useCancellablePromise();
   const [apiKey, setApiKey] = useState();
-  const mapProps = { center: mapCenter(), traffic: false, zoom: 4, geolocation: false, clickableIcons: false }
+  const [directionPlugin, setDirectionPlugin] = useState();
+  const mapProps = {
+    center: mapCenter(),
+    traffic: false, zoom: 4, geolocation: false, clickableIcons: false
+  }
   let mapObject;
   let mapplsClassObject = new mappls();
   let mapplsPluginObject = new mappls_plugin();
-  let add, direction_plugin, c = 0, ll = [
-    { lat: 28.63124010064198, lng: 77.46734619140625 },
-    { lat: 28.63395214251842, lng: 77.4635696411133 },
-    { lat: 28.634253476178397, lng: 77.45704650878908 },
-    { lat: 28.634856140902432, lng: 77.44880676269533 },
-    { lat: 28.635760131498788, lng: 77.44228363037111 },
-    { lat: 28.637266765186347, lng: 77.43679046630861 },
-    { lat: 28.637869412604015, lng: 77.43232727050783 },
-    { lat: 28.639677334088308, lng: 77.42855072021486 },
-    { lat: 28.640279967660007, lng: 77.42305755615236 },
-    { lat: 28.640882597770116, lng: 77.41928100585939 },
-    { lat: 28.640882597770116, lng: 77.41516113281251 },
-    { lat: 28.640581283147768, lng: 77.40932464599611 },
-    { lat: 28.63756808932784, lng: 77.40108489990236 },
-    { lat: 28.635760131498788, lng: 77.39421844482423 },
-    { lat: 28.634253476178397, lng: 77.38735198974611 },
-    { lat: 28.631541442089226, lng: 77.37808227539064 },
-  ];
+  let direction_plugin;
 
-  console.log("geoPositionStart=====>", geoPositionStart);
-  console.log("geoPositionEnd=====>", geoPositionEnd);
+  const onChangeTrack = (directionPluginData, location) => {
+    directionPluginData.tracking({
+      location: location,
+      label: 'current location',
+      icon: "https://apis.mapmyindia.com/map_v3/2.png",
+      heading: false,
+      reRoute: true,
+      fitBounds: false,
+      animationSpeed: 5,
+      delay: 2000
+    });
+  };
 
   const onMapLoad = () => {
     var direction_option = {
       map: mapObject,
       divWidth: '0px',
-      start: { label: 'start', geoposition: "28.63124010064198,77.46734619140625" },
-      end: { label: 'end', geoposition: "28.631541442089226,77.37808227539064" },
-      // start: { label: 'start', geoposition: geoPositionStart },
-      // end: { label: 'end', geoposition: geoPositionEnd },
+      start: { label: 'start', geoposition: geoPositionStart },
+      end: { label: 'end', geoposition: geoPositionEnd },
       steps: false,
       search: true,
       isDraggable: false,
       alternatives: false,
-      Resource: 'route_eta',
-      annotations: 'nodes',
       callback: function (data) { }
     };
 
-    // const defaultCenter = mapCenter();
-    // const defaultCurrentLocation = defaultCenter ? {
-    //   lat: parseFloat(defaultCenter[0]),
-    //   lng: parseFloat(defaultCenter[1]),
-    // } : null;
-    // if (currentLocation || defaultCurrentLocation) {
-    //   console.log("currentLocation 11111111111111=====>", currentLocation);
-    //   console.log("defaultCurrentLocation 2222222222222=====>", defaultCurrentLocation);
-    //   const location = currentLocation ? currentLocation : defaultCurrentLocation;
-    //   console.log("location 333333333333====>", location);
-    //   direction_plugin = mapplsPluginObject.direction(direction_option);
-    //   direction_plugin.tracking({
-    //     // location: { lat: 28.63124010064198, lng: 77.46734619140625 },
-    //     location: location,
-    //     label: 'current location',
-    //     icon: "https://apis.mapmyindia.com/map_v3/2.png",
-    //     heading: false,
-    //     reRoute: true,
-    //     fitBounds: false,
-    //     animationSpeed: 5,
-    //     delay: 2000
-    //   });
-    // }
+    const defaultCenter = mapCenter();
+    const defaultCurrentLocation = defaultCenter ? {
+      lat: parseFloat(defaultCenter[0]),
+      lng: parseFloat(defaultCenter[1]),
+    } : null;
 
     direction_plugin = mapplsPluginObject.direction(direction_option);
-    add = setInterval(() => {
-      c++;
-      if (ll[c]) {
-        direction_plugin.tracking({
-          location: ll[c],
-          label: 'current location',
-          icon: "https://apis.mapmyindia.com/map_v3/2.png",
-          heading: false,
-          reRoute: true,
-          fitBounds: false,
-          animationSpeed: 5,
-          delay: 2000
-        });
-        if (ll[c].lat === 28.631541442089226) {
-          // clearInterval(add);
-          // setTimeout(() => { alert("reached."); }, 500);
-        }
-      }
+    setDirectionPlugin(direction_plugin);
+    setTimeout(() => {
+      onChangeTrack(direction_plugin, currentLocation ? currentLocation : defaultCurrentLocation)
     }, 2500);
   };
 
@@ -126,7 +84,6 @@ export default function TrakingMapComponent(props) {
 
   const getToken = async () => {
     const res = await cancellablePromise(getCall(`/clientApis/v2/map/accesstoken`));
-    console.log("data: ", res);
     setApiKey(res.access_token);
   };
 
@@ -135,11 +92,9 @@ export default function TrakingMapComponent(props) {
     getToken();
   }, []);
 
-  console.log("currentLocation=====>", currentLocation);
   useEffect(() => {
     if (currentLocation) {
-      onMapLoad()
-      // onInitializeMap();
+      onChangeTrack(directionPlugin, currentLocation);
     }
   }, [currentLocation]);
 
--- src/components/orders/orderDetails/trackingMap.js
+++ src/components/orders/orderDetails/trackingMap.js
@@ -5,12 +5,6 @@ import TrakingMap from "../../common/TrakingMap/TrakingMap";
 
 const TrackingMapComponant = ({ orderDetails, trakingDetails }) => {
     const classes = useStyles();
-    let locationString = "28.679076630288467,77.06970870494843";
-    locationString = locationString.split(',');
-    const gps = {
-        lat: locationString[0],
-        lng: locationString[1]
-    }
 
     const [deliveryDetails, setDeliveryDetails] = useState(null);
     const [trakData, setTrakData] = useState(null);
@@ -30,11 +24,12 @@ const TrackingMapComponant = ({ orderDetails, trakingDetails }) => {
     useEffect(() => {
         if (trakingDetails) {
             let data = Object.assign({}, JSON.parse(JSON.stringify(trakingDetails)));
-            let locationString = data.location.gps;
+            // let locationString = "30.749469, 76.642282".replaceAll(' ', '');//data.location.gps.replaceAll(' ', '');
+            let locationString = data.location.gps.replaceAll(' ', '');
             locationString = locationString.split(',');
             data.location.latlng = {
-                lat: locationString[0],
-                lng: locationString[1]
+                lat: parseFloat(locationString[0]),
+                lng: parseFloat(locationString[1])
             };
             setTrakData(data);
         }
@@ -48,7 +43,7 @@ const TrackingMapComponant = ({ orderDetails, trakingDetails }) => {
                         mapCenter={() => {
                             let locationString = deliveryDetails.start.location.gps;
                             locationString = locationString.split(',');
-                            return [locationString[0], locationString[1]]
+                            return [parseFloat(locationString[0]), parseFloat(locationString[1])]
                         }}
                         geoPositionStart={deliveryDetails.start.location.gps.replaceAll(' ', '')}
                         geoPositionEnd={deliveryDetails.end.location.gps.replaceAll(' ', '')}
