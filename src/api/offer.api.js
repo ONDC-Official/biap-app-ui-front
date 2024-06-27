67145f47d0bfdac04bf3c4b591c5f9cdd070af56
Shalaka Patil <shalu214patil@gmail.com>
2024-06-27 09:13:57 +0530

    Update offers url

--- src/api/offer.api.js
+++ src/api/offer.api.js
@@ -4,12 +4,23 @@ import { getCall } from "./axios";
  * function to get all brands
  * @returns
  */
-export const getAllOffersRequest = (domain = "", lat, lng, provider = "", location = "") => {
+export const getAllOffersRequest = (
+  domain = "",
+  lat,
+  lng,
+  provider = "",
+  location = ""
+) => {
   return new Promise(async (resolve, reject) => {
     try {
-      let url = `/protocol/location-offers?latitude=${lat}&longitude=${lng}${domain ? `&domain=${domain}` : ""}${provider ? `&provider=${provider}` : ""}${location ? `&location=${location}` : ""}`;
+      let url = `/clientApis/v2/offers?latitude=${lat}&longitude=${lng}${
+        domain ? `&domain=${domain}` : ""
+      }${provider ? `&provider=${provider}` : ""}${
+        location ? `&location=${location}` : ""
+      }`;
       const data = await getCall(url);
-      return resolve(data.data);
+      console.log(data);
+      return resolve(data.response.data);
     } catch (err) {
       return reject(err);
     }
